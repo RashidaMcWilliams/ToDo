d4430c17647fe7fb5cfd02dbd8a8a37967e2ec7d
McShi <rashida.mcwilliams@gmail.com>
2017-10-27 10:23:35 -0400

    Added item class

---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime ItemDueDate { get; set; }
+        public string ItemDetails { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
